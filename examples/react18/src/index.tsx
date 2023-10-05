61c9d843bbfb43e99713099f2a4fb4b63c575a84
Andreas Karlsson <andreas.karlsson.se@gmail.com>
2023-10-05 06:37:07 +0200

    chore(examples): use typescript in react18 example

--- examples/react18/src/index.tsx
+++ examples/react18/src/index.tsx
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
