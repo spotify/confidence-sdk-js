ba81430a801db9a5ef72f6621638535b9e7034e2
Andreas Karlsson <andreas.karlsson.se@gmail.com>
2023-10-10 01:11:27 +0200

    fix(next): client setup sets features store

--- packages/integration-next/src/ClientSetup.tsx
+++ packages/integration-next/src/ClientSetup.tsx
@@ -1,8 +1,15 @@
 'use client';
 import React, { useMemo } from 'react';
-import { DefaultLogger, OpenFeature, OpenFeatureClient, OpenFeatureEventEmitter } from '@openfeature/web-sdk';
+import { EvaluationDetails, Features, JsonValue, Logger, OpenFeature } from '@openfeature/web-sdk';
 import { ConfidenceWebProvider, createConfidenceWebProvider } from '@spotify-confidence/openfeature-web-provider';
-import { OpenFeatureContextProvider } from '@spotify-confidence/integration-react';
+import { FeatureStoreContext, OpenFeatureContextProvider } from '@spotify-confidence/integration-react';
+
+const noopLogger: Logger = {
+  error: () => {},
+  info: () => {},
+  warn: () => {},
+  debug: () => {},
+};
 
 type RequiredSelected<T, K extends keyof T> = {
   [P in K]-?: NonNullable<T[P]>;
@@ -14,11 +21,18 @@ export interface ClientSetupProps {
     'initConfiguration'
   >;
   isServer?: boolean;
+  clientName?: string;
   fallback: React.ReactNode;
 }
 
 export function ClientSetup(props: React.PropsWithChildren<ClientSetupProps>) {
-  const { clientProviderFactoryOptions, isServer = typeof window === 'undefined', children, fallback } = props;
+  const {
+    clientProviderFactoryOptions,
+    isServer = typeof window === 'undefined',
+    children,
+    fallback,
+    clientName,
+  } = props;
 
   const clientProvider = useMemo((): ConfidenceWebProvider => {
     if (isServer) {
@@ -41,23 +55,107 @@ export function ClientSetup(props: React.PropsWithChildren<ClientSetupProps>) {
   }, [isServer, clientProvider, clientProviderFactoryOptions.initConfiguration.context]);
 
   if (isServer) {
-    const ssrClient = new OpenFeatureClient(
-      () => clientProvider,
-      () => new OpenFeatureEventEmitter(),
-      () => new DefaultLogger(),
-      {},
-    );
-
+    const store = {
+      subscribe: () => {
+        throw new Error('Subscription not supported in SSR');
+      },
+      getSnapshot: () => new SSRFeatures(clientProvider),
+    };
     return (
-      <OpenFeatureContextProvider client={ssrClient} providerReady>
+      <FeatureStoreContext.Provider value={store}>
         <React.Suspense fallback={fallback}>{children}</React.Suspense>
-      </OpenFeatureContextProvider>
+      </FeatureStoreContext.Provider>
     );
   }
 
   return (
-    <OpenFeatureContextProvider providerReady>
+    <OpenFeatureContextProvider client={OpenFeature.getClient(clientName)}>
       <React.Suspense fallback={fallback}>{children}</React.Suspense>
     </OpenFeatureContextProvider>
   );
+}
+
+class SSRFeatures implements Features {
+  constructor(private provider: ConfidenceWebProvider) {}
+
+  getBooleanValue(flagKey: string, defaultValue: boolean): boolean {
+    return this.provider.resolveBooleanEvaluation(
+      flagKey,
+      defaultValue,
+      this.provider.configuration?.context!,
+      noopLogger,
+    ).value;
+  }
+  getBooleanDetails(flagKey: string, defaultValue: boolean): EvaluationDetails<boolean> {
+    return {
+      flagKey,
+      flagMetadata: {},
+      ...this.provider.resolveBooleanEvaluation(
+        flagKey,
+        defaultValue,
+        this.provider.configuration?.context!,
+        noopLogger,
+      ),
+    };
+  }
+  getStringValue(flagKey: string, defaultValue: string): string {
+    return this.provider.resolveStringEvaluation(
+      flagKey,
+      defaultValue,
+      this.provider.configuration?.context!,
+      noopLogger,
+    ).value;
+  }
+  getStringDetails(flagKey: string, defaultValue: string): EvaluationDetails<string> {
+    return {
+      flagKey,
+      flagMetadata: {},
+      ...this.provider.resolveStringEvaluation(
+        flagKey,
+        defaultValue,
+        this.provider.configuration?.context!,
+        noopLogger,
+      ),
+    };
+  }
+  getNumberValue(flagKey: string, defaultValue: number): number {
+    return this.provider.resolveNumberEvaluation(
+      flagKey,
+      defaultValue,
+      this.provider.configuration?.context!,
+      noopLogger,
+    ).value;
+  }
+  getNumberDetails(flagKey: string, defaultValue: number): EvaluationDetails<number> {
+    return {
+      flagKey,
+      flagMetadata: {},
+      ...this.provider.resolveNumberEvaluation(
+        flagKey,
+        defaultValue,
+        this.provider.configuration?.context!,
+        noopLogger,
+      ),
+    };
+  }
+  getObjectValue(flagKey: string, defaultValue: JsonValue): JsonValue {
+    return this.provider.resolveObjectEvaluation(
+      flagKey,
+      defaultValue,
+      this.provider.configuration?.context!,
+      noopLogger,
+    ).value;
+  }
+  getObjectDetails(flagKey: string, defaultValue: JsonValue): EvaluationDetails<JsonValue> {
+    return {
+      flagKey,
+      flagMetadata: {},
+      ...this.provider.resolveObjectEvaluation(
+        flagKey,
+        defaultValue,
+        this.provider.configuration?.context!,
+        noopLogger,
+      ),
+    };
+  }
 }
