13f90dcefa7450f8a702c8ad4a728567f6078ebc
Bill Lynch <llynch.bill@gmail.com>
2023-10-12 11:20:28 +0100

    feat(examples): add node example

---
+++ examples/node/src/index.js
@@ -0,0 +1,20 @@
+import { createConfidenceServerProvider } from '@spotify-confidence/openfeature-server-provider';
+import { OpenFeature } from '@openfeature/js-sdk';
+
+const provider = createConfidenceServerProvider({
+  clientSecret: 'RxDVTrXvc6op1XxiQ4OaR31dKbJ39aYV',
+  region: 'eu',
+  fetchImplementation: fetch,
+});
+
+OpenFeature.setProvider(provider);
+
+const client = OpenFeature.getClient();
+
+client
+  .getBooleanValue('web-sdk-e2e-flag.bool', false, {
+    targetingKey: `user-${Math.random()}`,
+  })
+  .then(result => {
+    console.log('result:', result);
+  });
