2lambda123/confidence-openfeature-provider-js

feat(examples): add next 12 and next 13 example apps

examples/next12/pages/next12/index.tsx
@@ -1,0 +1,29 @@
+import React from 'react';
+import { OpenFeature } from '@openfeature/js-sdk';
+import { setupOpenFeatureConfidenceProvider } from '../../utils/setupOpenFeatureConfidenceProvider';
+
+setupOpenFeatureConfidenceProvider();
+
+interface Next12StyleComponentProps {
+  string: string;
+}
+export default function Next12StyleComponent(props: React.PropsWithChildren<Next12StyleComponentProps>) {
+  return <p>Value for the string from getServerSideProps: {props.string}</p>;
+}
+
+export async function getServerSideProps() {
+  const boolToUseOnClientSide = await OpenFeature.getClient().getBooleanValue('web-sdk-e2e-flag.bool', true, {
+    targetingKey: 'user-a',
+  });
+
+  if (boolToUseOnClientSide) {
+    // eslint-disable-next-line no-console
+    console.log('Do something with experimentation on the server side!');
+  }
+
+  const stringValueForClient = await OpenFeature.getClient().getStringValue('web-sdk-e2e-flag.str', 'default', {
+    targetingKey: 'user-a',
+  });
+
+  return { props: { string: stringValueForClient } };
+}

examples/next13/pages/next12Simulation/index.tsx
@@ -1,0 +1,29 @@
+import React from 'react';
+import { OpenFeature } from '@openfeature/js-sdk';
+import { setupOpenFeatureConfidenceProvider } from '@/utils/setupOpenFeatureConfidenceProvider';
+
+setupOpenFeatureConfidenceProvider();
+
+interface Next12StyleComponentProps {
+  string: string;
+}
+export default function Next12StyleComponent(props: React.PropsWithChildren<Next12StyleComponentProps>) {
+  return <p>Value for the string from getServerSideProps: {props.string}</p>;
+}
+
+export async function getServerSideProps() {
+  const boolToUseOnClientSide = await OpenFeature.getClient().getBooleanValue('web-sdk-e2e-flag.bool', true, {
+    targetingKey: 'user-a',
+  });
+
+  if (boolToUseOnClientSide) {
+    // eslint-disable-next-line no-console
+    console.log('Do something with experimentation on the server side!');
+  }
+
+  const stringValueForClient = await OpenFeature.getClient().getStringValue('web-sdk-e2e-flag.str', 'default', {
+    targetingKey: 'user-a',
+  });
+
+  return { props: { string: stringValueForClient } };
+}

spotify/confidence-sdk-js

examples/next12/pages/next12/index.tsx
@@ -1,0 +1,29 @@
+import React from 'react';
+import { OpenFeature } from '@openfeature/js-sdk';
+import { setupOpenFeatureConfidenceProvider } from '../../utils/setupOpenFeatureConfidenceProvider';
+
+setupOpenFeatureConfidenceProvider();
+
+interface Next12StyleComponentProps {
+  string: string;
+}
+export default function Next12StyleComponent(props: React.PropsWithChildren<Next12StyleComponentProps>) {
+  return <p>Value for the string from getServerSideProps: {props.string}</p>;
+}
+
+export async function getServerSideProps() {
+  const boolToUseOnServerSide = await OpenFeature.getClient().getBooleanValue('web-sdk-e2e-flag.bool', true, {
+    targetingKey: 'user-a',
+  });
+
+  if (boolToUseOnServerSide) {
+    // eslint-disable-next-line no-console
+    console.log('Do something with experimentation on the server side!');
+  }
+
+  const stringValueForClient = await OpenFeature.getClient().getStringValue('web-sdk-e2e-flag.str', 'default', {
+    targetingKey: 'user-a',
+  });
+
+  return { props: { string: stringValueForClient } };
+}

examples/next13/pages/next12Simulation/index.tsx
@@ -1,0 +1,29 @@
+import React from 'react';
+import { OpenFeature } from '@openfeature/js-sdk';
+import { setupOpenFeatureConfidenceProvider } from '@/utils/setupOpenFeatureConfidenceProvider';
+
+setupOpenFeatureConfidenceProvider();
+
+interface Next12StyleComponentProps {
+  string: string;
+}
+export default function Next12StyleComponent(props: React.PropsWithChildren<Next12StyleComponentProps>) {
+  return <p>Value for the string from getServerSideProps: {props.string}</p>;
+}
+
+export async function getServerSideProps() {
+  const boolToUseOnServerSide = await OpenFeature.getClient().getBooleanValue('web-sdk-e2e-flag.bool', true, {
+    targetingKey: 'user-a',
+  });
+
+  if (boolToUseOnServerSide) {
+    // eslint-disable-next-line no-console
+    console.log('Do something with experimentation on the server side!');
+  }
+
+  const stringValueForClient = await OpenFeature.getClient().getStringValue('web-sdk-e2e-flag.str', 'default', {
+    targetingKey: 'user-a',
+  });
+
+  return { props: { string: stringValueForClient } };
+}