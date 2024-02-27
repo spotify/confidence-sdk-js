9241161005e6b193c3a414dbe8116c352ef1a669
Andreas Karlsson <andreas.karlsson.se@gmail.com>
2024-02-27 10:52:55 +0100

    feat(client-http): improved request logic

--- packages/openfeature-web-provider/src/factory.ts
+++ packages/openfeature-web-provider/src/factory.ts
@@ -1,6 +1,6 @@
 import { Provider } from '@openfeature/web-sdk';
 import { ConfidenceWebProvider } from './ConfidenceWebProvider';
-import { ConfidenceClient, ConfidenceClientOptions } from '@spotify-confidence/client-http';
+import { ConfidenceClient, ConfidenceClientOptions, FetchBuilder, TimeUnit } from '@spotify-confidence/client-http';
 
 type ConfidenceWebProviderFactoryOptions = {
   region?: ConfidenceClientOptions['region'];
@@ -17,7 +17,7 @@ export function createConfidenceWebProvider({
 }: ConfidenceWebProviderFactoryOptions): Provider {
   const confidenceClient = new ConfidenceClient({
     ...options,
-    fetchImplementation,
+    fetchImplementation: withRequestLogic(fetchImplementation),
     apply: options.apply === 'backend',
     sdk: {
       id: 'SDK_ID_JS_WEB_PROVIDER',
@@ -28,6 +28,42 @@ export function createConfidenceWebProvider({
   return new ConfidenceWebProvider(confidenceClient, {
     apply: options.apply || 'access',
   });
+}
+
+// exported for testing
+export function withRequestLogic(fetchImplementation: (request: Request) => Promise<Response>): typeof fetch {
+  const fetchResolve = new FetchBuilder()
+    // always cancel previous resolve
+    .abortPrevious()
+    // infinite retries without delay until aborted by timeout
+    .retry()
+    .rejectNotOk()
+    .rateLimit(1, { initialTokens: 3, maxTokens: 2 })
+    .build(fetchImplementation);
+
+  const fetchApply = new FetchBuilder()
+    .limitPending(1000)
+    .timeout(30 * TimeUnit.MINUTE)
+    .retry({ delay: 5 * TimeUnit.SECOND, backoff: 2, maxDelay: 5 * TimeUnit.MINUTE, jitter: 0.2 })
+    .rejectNotOk()
+    .rateLimit(2)
+    // update send-time before sending
+    .modifyRequest(async request => {
+      if (request.method === 'POST') {
+        const body = JSON.stringify({ ...(await request.json()), sendTime: new Date().toISOString() });
+        return new Request(request, { body });
+      }
+      return request;
+    })
+    .build(fetchImplementation);
+
+  return (
+    new FetchBuilder()
+      .route(url => url.endsWith('flags:resolve'), fetchResolve)
+      .route(url => url.endsWith('flags:apply'), fetchApply)
+      // throw so we notice changes in endpoints that should be handled here
+      .build(request => Promise.reject(new Error(`Unexpected url: ${request.url}`)))
+  );
 }
 
 function defaultFetchImplementation(): typeof fetch {
