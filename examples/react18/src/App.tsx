7f5316c0eb468f980c4ad9415d39d1164281297f
vahid torkaman <vahidt@spotify.com>
2024-03-15 09:29:05 +0100

    update the demo react app to send events using confidence

--- examples/react18/src/App.tsx
+++ examples/react18/src/App.tsx
@@ -1,15 +1,16 @@
 import React, { useEffect } from 'react';
 import { ClientProviderEvents, OpenFeature } from '@openfeature/web-sdk';
 import TestComponent from './TestComponent';
+import { Confidence } from '@spotify-confidence/sdk';
 import { createConfidenceWebProvider } from '@spotify-confidence/openfeature-web-provider';
 
-export const webProvider = createConfidenceWebProvider({
+const confidence = Confidence.create({
   clientSecret: 'RxDVTrXvc6op1XxiQ4OaR31dKbJ39aYV',
-  region: 'eu',
-  fetchImplementation: window.fetch.bind(window),
-  apply: 'access',
+  environment: 'client',
   timeout: 1000,
 });
+
+export const webProvider = createConfidenceWebProvider(confidence);
 
 OpenFeature.getClient().addHandler(ClientProviderEvents.Ready, () => console.log('ready!'));
 function App() {
@@ -20,11 +21,13 @@ function App() {
     OpenFeature.setProvider(webProvider);
   }, []);
 
+  confidence.updateContext("page", "testComponent");
+
   return (
     <>
       <h1>React 18 Example</h1>
       <React.Suspense fallback={<p>Loading... </p>}>
-        <TestComponent />
+        <TestComponent eventSender={confidence}/>
       </React.Suspense>
     </>
   );
--- examples/react18/src/TestComponent.tsx
+++ examples/react18/src/TestComponent.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useStringValue } from '@spotify-confidence/integration-react';
 import { OpenFeature } from '@openfeature/web-sdk';
+import { EventSender } from '@spotify-confidence/sdk';
 
-export const TestComponent = () => {
+export const TestComponent = (props: { eventSender: EventSender }) => {
   const str = useStringValue('web-sdk-e2e-flag.str', 'default');
-
+  const { eventSender } = props;
   return (
     <>
       <p>The string flag value is: {str}</p>
       <button
         onClick={() => {
           OpenFeature.setContext({ targetingKey: `user-${Math.random()}` });
+          eventSender.sendEvent('eventDefinitions/navigate');
         }}
       >
         Randomise OpenFeature Context
