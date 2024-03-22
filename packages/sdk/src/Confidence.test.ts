f74f0cf3a3fb2da0eb202ac3f5a6cccdf2983b4f
Nicky Bondarenko <nickyb@spotify.com>
2024-03-22 14:36:04 +0100

    test: added a few tests

--- packages/sdk/src/Confidence.test.ts
+++ packages/sdk/src/Confidence.test.ts
@@ -20,4 +20,50 @@ describe('Confidence', () => {
       expect(confidence.getContext()).toEqual({ clothes: { pants: 'yellow' } });
     });
   });
+  describe('setContext', () => {
+    it('sets context', () => {
+      const confidence = new Confidence({} as any);
+      const newContext = {
+        pants: 'yellow',
+      };
+      confidence.setContext(newContext);
+      expect(confidence.getContext()).toEqual(newContext);
+    });
+  });
+  describe('withContext', () => {
+    it('creates a child context', () => {
+      const parent = new Confidence({} as any);
+      const additionalContext = {
+        clothes: 'pants',
+      };
+      let child = parent.withContext(additionalContext);
+      expect(child.getContext()).toEqual(additionalContext);
+    });
+    it('merge contexts', () => {
+      const parent = new Confidence({} as any);
+      parent.setContext({
+        clothes: 'pants',
+      });
+      const child = parent.withContext({
+        pants: 'blue',
+      });
+      expect(child.getContext()).toEqual({
+        clothes: 'pants',
+        pants: 'blue',
+      });
+    });
+  });
+  describe('create', () => {
+    it('creates a new confidence object', () => {
+      const confidenceProvider = Confidence.create({
+        clientSecret: 'secret',
+        region: 'us',
+        baseUrl: 'https://www.spotify.com',
+        environment: 'client',
+        fetchImplementation: {} as any,
+        timeout: 10,
+      });
+      expect(confidenceProvider.getContext()).toEqual({});
+    });
+  });
 });
