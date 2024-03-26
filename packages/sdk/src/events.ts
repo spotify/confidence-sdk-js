7a6c2eeeb19f761a4353fe0b7418f044a3f5c48c
Nicky Bondarenko <nickyb@spotify.com>
2024-03-26 15:09:41 +0100

    refactor: refactor tests, align api

--- packages/sdk/src/events.ts
+++ packages/sdk/src/events.ts
@@ -2,5 +2,5 @@ import { Value } from './Value';
 import { Contextual } from './context';
 
 export interface EventSender extends Contextual<EventSender> {
-  sendEvent(name: string, message?: Value): void;
+  sendEvent(name: string, message?: Value.Struct): void;
 }
