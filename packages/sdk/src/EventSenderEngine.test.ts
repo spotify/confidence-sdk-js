5cd8b22bb9e9be5e123d6c76223b6fb28732344a
Nicklas Lundin <nicklasl@spotify.com>
2024-04-03 13:09:33 +0200

    feat: Total confidence fake PR (#84)

---
+++ packages/sdk/src/EventSenderEngine.test.ts
@@ -0,0 +1,64 @@
+import { EventSenderEngine } from './EventSenderEngine';
+
+jest.useFakeTimers();
+
+const BATCH_SIZE = 10;
+const MAX_OPEN_REQUESTS = 10;
+const UPLOAD_LATENCY = 10;
+const FLUSH_TIMEOUT = 10;
+
+describe('EventSenderEngine unit tests', () => {
+  const mockFetch = jest.fn(async () => {
+    await sleep(UPLOAD_LATENCY);
+    return new Response(JSON.stringify({ errors: [] }));
+  });
+  const engine = new EventSenderEngine({
+    clientSecret: 'some client secret',
+    maxBatchSize: BATCH_SIZE,
+    flushTimeoutMilliseconds: FLUSH_TIMEOUT,
+    fetchImplementation: mockFetch as any,
+    region: 'eu',
+    maxOpenRequests: MAX_OPEN_REQUESTS,
+    logger: {},
+  });
+  const flushSpy = jest.spyOn(engine, 'flush');
+  it(`should flush ${FLUSH_TIMEOUT} millisecond after the last send`, async () => {
+    engine.send({}, 'some-event');
+    await jest.advanceTimersByTimeAsync(FLUSH_TIMEOUT - 1);
+    expect(flushSpy).toHaveBeenCalledTimes(0);
+
+    engine.send({}, 'some-event');
+    await jest.advanceTimersByTimeAsync(FLUSH_TIMEOUT - 1);
+    expect(flushSpy).toHaveBeenCalledTimes(0);
+
+    await jest.advanceTimersByTimeAsync(1);
+    expect(flushSpy).toHaveBeenCalledTimes(1);
+  });
+  it('should flush once batch is full', async () => {
+    for (let i = 0; i <= BATCH_SIZE; i++) {
+      engine.send({ i: 'pants' }, 'event', { message: '' });
+    }
+    expect(flushSpy).toHaveBeenCalledTimes(1);
+    await jest.runAllTimersAsync();
+    expect(mockFetch).toHaveBeenCalledTimes(2);
+    expect(flushSpy).toHaveBeenCalledTimes(2);
+  });
+  it('should handle a lot of events', async () => {
+    const eventCount = BATCH_SIZE * MAX_OPEN_REQUESTS * 2;
+    for (let i = 0; i < eventCount; i++) {
+      engine.send({ i: 'pants' }, 'event', { message: '' });
+    }
+    await jest.runAllTimersAsync();
+    expect(flushSpy).toHaveBeenCalledTimes(eventCount / BATCH_SIZE);
+    expect(mockFetch).toHaveBeenCalledTimes(MAX_OPEN_REQUESTS);
+    const flushResults = await Promise.all(flushSpy.mock.results.map(x => x.value));
+    expect(flushResults.slice(0, MAX_OPEN_REQUESTS).every(x => x)).toBe(true);
+    expect(flushResults.slice(MAX_OPEN_REQUESTS).every(x => !x)).toBe(true);
+  });
+});
+
+function sleep(millis: number): Promise<void> {
+  return new Promise(resolve => {
+    setTimeout(resolve, millis);
+  });
+}
---
+++ packages/sdk/src/FlagResolverClient.ts
@@ -0,0 +1,81 @@
+import {
+  ConfidenceClient,
+  ConfidenceClientOptions,
+  FetchBuilder,
+  Configuration as FlagResolution,
+  TimeUnit,
+  ApplyManager,
+  abortableSleep,
+} from '@spotify-confidence/client-http';
+import { Context } from './context';
+
+const APPLY_TIMEOUT = 250;
+const MAX_APPLY_BUFFER_SIZE = 20;
+export interface FlagResolverOptions extends Omit<ConfidenceClientOptions, 'apply'> {
+  environment: 'client' | 'backend';
+}
+
+class FlagResolverClient {
+  private readonly legacyClient: ConfidenceClient;
+  private readonly applyManager?: ApplyManager;
+
+  constructor({ fetchImplementation, environment, ...options }: FlagResolverOptions) {
+    this.legacyClient = new ConfidenceClient({
+      ...options,
+      apply: environment === 'backend',
+      fetchImplementation: environment === 'client' ? withRequestLogic(fetchImplementation) : fetchImplementation,
+    });
+    if (environment === 'client') {
+      this.applyManager = new ApplyManager({
+        client: this.legacyClient,
+        timeout: APPLY_TIMEOUT,
+        maxBufferSize: MAX_APPLY_BUFFER_SIZE,
+      });
+    }
+  }
+
+  resolve({ openFeature, ...context }: Context, flags: string[]): Promise<FlagResolution> {
+    return this.legacyClient.resolve({ ...context, ...openFeature }, { flags });
+  }
+
+  apply(resolveToken: string, flagName: string): void {
+    this.applyManager?.apply(resolveToken, flagName);
+  }
+}
+
+export { FlagResolverClient, FlagResolution, abortableSleep };
+
+export function withRequestLogic(fetchImplementation: (request: Request) => Promise<Response>): typeof fetch {
+  const fetchResolve = new FetchBuilder()
+    // always cancel previous resolve
+    .abortPrevious()
+    // infinite retries without delay until aborted by timeout
+    .retry()
+    .rejectNotOk()
+    .rateLimit(1, { initialTokens: 3, maxTokens: 2 })
+    .build(fetchImplementation);
+
+  const fetchApply = new FetchBuilder()
+    .limitPending(1000)
+    .timeout(30 * TimeUnit.MINUTE)
+    .retry({ delay: 5 * TimeUnit.SECOND, backoff: 2, maxDelay: 5 * TimeUnit.MINUTE, jitter: 0.2 })
+    .rejectNotOk()
+    .rateLimit(2)
+    // update send-time before sending
+    .modifyRequest(async request => {
+      if (request.method === 'POST') {
+        const body = JSON.stringify({ ...(await request.json()), sendTime: new Date().toISOString() });
+        return new Request(request, { body });
+      }
+      return request;
+    })
+    .build(fetchImplementation);
+
+  return (
+    new FetchBuilder()
+      .route(url => url.endsWith('flags:resolve'), fetchResolve)
+      .route(url => url.endsWith('flags:apply'), fetchApply)
+      // throw so we notice changes in endpoints that should be handled here
+      .build(request => Promise.reject(new Error(`Unexpected url: ${request.url}`)))
+  );
+}
