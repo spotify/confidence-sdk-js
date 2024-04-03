b452fbb82fb01d35cec4f0ebd0a82e615756ee5e
Nicklas Lundin <nicklasl@spotify.com>
2024-04-03 13:09:33 +0200

    feat: Total confidence fake PR (#84)

---
+++ packages/sdk/src/Value.ts
@@ -0,0 +1,31 @@
+export namespace Value {
+  export type Primitive = number | string | boolean | undefined;
+  export type Struct = {
+    readonly [key: string]: Value;
+  };
+  export type List = ReadonlyArray<Value>;
+
+  export function clone<T extends Value>(value: T): T {
+    if (isStruct(value)) {
+      const cloned: Record<string, Value> = {};
+      for (const key of Object.keys(value)) {
+        cloned[key] = clone(value[key]);
+      }
+      return Object.freeze(cloned) as T;
+    }
+    if (isList(value)) {
+      return Object.freeze(value.map(clone)) as T;
+    }
+    return value;
+  }
+
+  export function isStruct(value: Value): value is Struct {
+    return typeof value === 'object' && value !== null && !Array.isArray(value);
+  }
+
+  export function isList(value: Value): value is List {
+    return typeof value === 'object' && value !== null && Array.isArray(value);
+  }
+}
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export type Value = Value.Primitive | Value.Struct | Value.List;
