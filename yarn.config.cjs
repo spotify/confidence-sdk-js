09af9559483066fc3c27bb6cd548e4257bb0f198
Andreas Karlsson <andreas.karlsson.se@gmail.com>
2024-04-15 02:14:19 +0200

    build: yarn constraints

---
+++ yarn.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import('@yarnpkg/types')} */
+const { defineConfig } = require('@yarnpkg/types');
+
+module.exports = defineConfig({
+  async constraints({ Yarn }) {
+    for (const workspace of Yarn.workspaces()) {
+      if (!workspace.cwd.startsWith('packages/')) continue;
+      workspace.set('type', 'module');
+      workspace.set('module', 'build/esm/index.js');
+      workspace.set('types', 'build/types/index.d.ts');
+      workspace.set('files', ['dist/*']);
+      workspace.set('scripts.build', 'tsc');
+      workspace.set('scripts.bundle', 'api-extractor run ${CI:---local} && rollup -c');
+      workspace.set('publishConfig', {
+        access: 'public',
+        types: 'dist/index.d.ts',
+        module: 'dist/index.js',
+      });
+      workspace.set('devDependencies.@microsoft/api-extractor', '*');
+      workspace.set('devDependencies.rollup', '*');
+
+      // make sure all deps to this workspace use the correct version
+      for (const dep of Yarn.dependencies({ ident: workspace.ident })) {
+        dep.update(workspace.pkg.version);
+      }
+    }
+  },
+});
