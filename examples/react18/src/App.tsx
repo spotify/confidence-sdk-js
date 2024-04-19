4d15b3194bfeb2b075e1a742554623b65c598beb
Andreas Karlsson <andreas.karlsson.se@gmail.com>
2024-04-19 09:37:30 +0200

    fix: remove all confidence code for demo

--- examples/react18/src/App.tsx
+++ examples/react18/src/App.tsx
@@ -1,57 +1,21 @@
-import React, { useEffect } from 'react';
-import { ClientProviderEvents, OpenFeature } from '@openfeature/web-sdk';
+import React from 'react';
 import TestComponent from './TestComponent';
-import { createConfidenceWebProvider } from '@spotify-confidence/openfeature-web-provider';
-import { Confidence, pageViews, webVitals } from '@spotify-confidence/sdk';
-import { FetchBuilder } from '@spotify-confidence/client-http';
-import { ConfidenceProvider } from './ConfidenceContext';
 
-const fetchImplementation = new FetchBuilder()
-  .route(
-    url => url.endsWith('/events:publish'),
-    async req => {
-      const { events } = await req.json();
-      // @ts-ignore
-      for (const { eventDefinition, payload } of events) {
-        console.log(eventDefinition.split('/')[1], payload);
-      }
-      return new Response(JSON.stringify({ errors: [] }));
-    },
-  )
-  .build(fetch.bind(window));
-
-const confidence = Confidence.create({
-  clientSecret: 'RxDVTrXvc6op1XxiQ4OaR31dKbJ39aYV',
-  region: 'eu',
-  environment: 'client',
-  fetchImplementation,
-  timeout: 1000,
-});
-
-confidence.track(webVitals());
-
-export const webProvider = createConfidenceWebProvider(confidence);
-
-OpenFeature.getClient().addHandler(ClientProviderEvents.Ready, () => console.log('ready!'));
 function App() {
-  useEffect(() => {
-    // OpenFeature.setContext({
-    //   targetingKey: 'user-a',
-    // });
-    OpenFeature.setProvider(webProvider);
-    // confidence.sendEvent('test');
-  }, []);
-
   return (
-    <ConfidenceProvider confidence={confidence}>
+    <>
       <h1>React 18 Example</h1>
-      <div style={{ height: 2000 }}>
+      <div>
         <React.Suspense fallback={<p>Loading... </p>}>
           <TestComponent />
         </React.Suspense>
       </div>
+      <p>
+        // TODO wrap this amazing new kitten feature in a feature flag so it doesn't show up in production
+        <img src="https://thepetshow.com/wp-content/uploads/2015/07/lion-mirror.jpg" alt="confident kitten" />
+      </p>
       <p>bottom</p>
-    </ConfidenceProvider>
+    </>
   );
 }
 
--- examples/react18/src/TestComponent.tsx
+++ examples/react18/src/TestComponent.tsx
@@ -1,29 +1,16 @@
-import React from 'react';
-import { useStringValue } from '@spotify-confidence/integration-react';
-import { OpenFeature } from '@openfeature/web-sdk';
-import { useConfidence, withContext } from './ConfidenceContext';
-
 const TestComponent = () => {
-  const str = useStringValue('web-sdk-e2e-flag.str', 'default');
-  const confidence = useConfidence();
   return (
     <>
-      <p>The string flag value is: {str}</p>
       <button
         onClick={() => {
-          OpenFeature.setContext({ targetingKey: `user-${Math.random()}` });
-          confidence.sendEvent('click');
-          confidence.close();
+          document.cookie = `cnfdVisitorId=; expires=${new Date(0).toUTCString()}`;
+          document.location.reload();
         }}
       >
-        Randomize OpenFeature Context
+        Clear cookies and reload
       </button>
-      <p>
-        // TODO wrap this amazing new kitten feature in a feature flag so it doesn't show up in production
-        <img src="https://thepetshow.com/wp-content/uploads/2015/07/lion-mirror.jpg" alt="confident kitten" />
-      </p>
     </>
   );
 };
 
-export default withContext({ component: 'TestComponent' })(TestComponent);
+export default TestComponent;
