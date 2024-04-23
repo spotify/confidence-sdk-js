b5e4b76616fc05b2a9bd0cc48bee7ad154cea7f5
Nicklas Lundin <nicklasl@spotify.com>
2024-04-23 11:04:12 +0200

    refactor: drop the "intercept" of send event request

--- examples/react18/src/App.tsx
+++ examples/react18/src/App.tsx
@@ -6,25 +6,10 @@ import { Confidence, pageViews } from '@spotify-confidence/sdk';
 import { FetchBuilder } from '@spotify-confidence/client-http';
 import { ConfidenceProvider } from './ConfidenceContext';
 
-const fetchImplementation = new FetchBuilder()
-  .route(
-    url => url.endsWith('/events:publish'),
-    async req => {
-      const { events } = await req.json();
-      // @ts-ignore
-      for (const { eventDefinition, payload } of events) {
-        console.log(eventDefinition.split('/')[1], payload);
-      }
-      return new Response(JSON.stringify({ errors: [] }));
-    },
-  )
-  .build(fetch.bind(window));
-
 const confidence = Confidence.create({
   clientSecret: 'RxDVTrXvc6op1XxiQ4OaR31dKbJ39aYV',
   region: 'eu',
   environment: 'client',
-  fetchImplementation,
   timeout: 1000,
 });
 
