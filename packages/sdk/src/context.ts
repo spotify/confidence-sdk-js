8b3d519f8331b31a4d5fdd9df29425aa39af8c8e
Nicklas Lundin <nicklasl@spotify.com>
2024-04-23 11:10:27 +0200

    refactor: remove user agent types for now

--- packages/sdk/src/context.ts
+++ packages/sdk/src/context.ts
@@ -7,20 +7,10 @@ export interface Contextual<Self extends Contextual<Self>> {
   clearContext(): void;
 }
 
-type Brand = {
-  brand: 'string';
-  version: number;
-};
-
 export interface Context extends Value.Struct {
   visitor_id?: string;
   openFeature?: Value.Struct & {
     targeting_key?: string;
-  };
-  userAgentData?: {
-    brands: Brand[];
-    mobile: boolean;
-    platform: string;
   };
   page?: {
     path: string;
