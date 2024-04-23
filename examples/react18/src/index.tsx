29022aa6662c03a5d72404098d8a29e13788854c
Nicklas Lundin <nicklasl@spotify.com>
2024-04-23 11:05:25 +0200

    refactor: remove debug GA integration

--- examples/react18/src/index.tsx
+++ examples/react18/src/index.tsx
@@ -4,27 +4,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-_setupGoogleAnalytics();
-
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
-
-function _setupGoogleAnalytics() {
-  const script = document.createElement('script');
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-J33MB49R7T';
-  script.async = true;
-  document.body.appendChild(script);
-
-  // @ts-ignore
-  window.dataLayer = [];
-  function gtag(...args: any[]) {
-    // @ts-ignore
-    dataLayer.push(arguments);
-  }
-  gtag('js', new Date());
-
-  gtag('config', 'G-J33MB49R7T');
-}
