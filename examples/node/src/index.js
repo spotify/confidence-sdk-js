3c654767bd63637c145c9a6f264f870c823e5645
Nicklas Lundin <nicklasl@spotify.com>
2024-05-21 08:52:25 +0200

    test: fixup node example

--- examples/node/src/index.js
+++ examples/node/src/index.js
@@ -6,23 +6,14 @@ const confidence = Confidence.create({
   timeout: 1000,
   logger: console,
 });
-// confidence.setContext()
 main();
 
 async function main() {
-  // confidence.subscribe(state => console.log('state:', state));
+  confidence.subscribe(state => console.log('state:', state));
   confidence.setContext({ targeting_key: 'user-a' });
-  const fe = await confidence.evaluateFlag('web-sdk-e2e-flag.int', 0);
+  const fe = confidence.evaluateFlag('web-sdk-e2e-flag.int', 0);
 
   console.log(fe);
-  confidence.setContext({ targeting_key: 'user-b' });
-  // console.log(await fe);
-}
 
-// client
-//   .getBooleanValue('web-sdk-e2e-flag.bool', false, {
-//     targetingKey: `user-${Math.random()}`,
-//   })
-//   .then(result => {
-//     console.log('result:', result);
-//   });
+  console.log(await fe);
+}
