e42ac300f5edf202461f64f75192e3afed6942ee
Fabrizio Demaria <fabrizio.f.demaria@gmail.com>
2024-08-07 16:58:56 +0200

    fix: wip: hash secret in visitor id

--- packages/sdk/src/trackers/visitorId.ts
+++ packages/sdk/src/trackers/visitorId.ts
@@ -1,9 +1,10 @@
 import { Trackable } from '../Trackable';
 import { uuid, Cookie } from '../utils';
+import * as CryptoJS from 'crypto-js';
 
 const COOKIE_NAME = 'cnfdVisitorId';
 
-export const visitorIdentity = (): Trackable.Manager => controller => {
+export const visitorIdentity = (clientSecret: string): Trackable.Manager => controller => {
   if (typeof document === 'undefined') return;
   let value = Cookie.get(COOKIE_NAME);
   if (!value) {
@@ -11,5 +12,8 @@ export const visitorIdentity = (): Trackable.Manager => controller => {
     // TODO check correct cookie options
     Cookie.set(COOKIE_NAME, value, { maxAge: 60 * 60 * 24 * 365 * 5 });
   }
-  controller.setContext({ visitor_id: value });
+
+  const combinedString = value + clientSecret;
+  const hashedValue = CryptoJS.SHA256(combinedString).toString();
+  controller.setContext({ visitor_id: hashedValue });
 };
