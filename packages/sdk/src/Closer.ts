d27d980e32e5008e3b36be8fec4ba0e5af17d85f
Nicky Bondarenko <nickyb@spotify.com>
2024-08-12 15:06:19 +0200

    fix: lint

--- packages/sdk/src/Closer.ts
+++ packages/sdk/src/Closer.ts
@@ -12,9 +12,10 @@ export namespace Closer {
     };
   }
 }
-// eslint-disable-next-line @typescript-eslint/no-redeclare
+
 /**
  * Utility functions to manage Closer types
  * @public
  */
+// eslint-disable-next-line @typescript-eslint/no-redeclare
 export type Closer = () => void;
