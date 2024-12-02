1ffe355a79c0f10bb0068888ae4a3bd6be1c57de
Nicky Bondarenko <nickyb@spotify.com>
2024-12-02 14:31:57 +0100

    fix: wording

--- packages/sdk/src/Confidence.ts
+++ packages/sdk/src/Confidence.ts
@@ -319,7 +319,7 @@ export class Confidence implements EventSender, Trackable, FlagResolver {
 
   private showLoggerLink(flag: string, context: Context) {
     this.config.logger.info?.(
-      `[Resolve debug] https://app.confidence.spotify.com/flags/resolver-test?client-key=${
+      `See resolves for '${flag}' in Confidence: https://app.confidence.spotify.com/flags/resolver-test?client-key=${
         this.config.clientSecret
       }&flag=flags/${flag}&context=${encodeURIComponent(JSON.stringify(context))}`,
     );
@@ -402,7 +402,7 @@ function defaultFetchImplementation(): typeof fetch {
 function defaultLogger(): Logger {
   try {
     if (process.env.NODE_ENV === 'development') {
-      return Logger.withLevel(console, 'debug');
+      return Logger.withLevel(console, 'info');
     }
   } catch (e) {
     // ignore
