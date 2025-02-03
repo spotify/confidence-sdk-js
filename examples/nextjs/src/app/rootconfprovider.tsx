58f948a1c330d335b5cfbddfd20af51f477863e5
Martin Svensson <msvens@spotify.com>
2025-02-03 19:52:56 +0100

    chore: fix format and other stuff again

--- examples/nextjs/src/app/rootconfprovider.tsx
+++ examples/nextjs/src/app/rootconfprovider.tsx
@@ -1,36 +1,31 @@
-'use client'
+'use client';
 
-import {ConfidenceProvider} from '@spotify-confidence/react';
-import {ReactNode} from "react";
-import {Confidence, visitorIdentity} from "@spotify-confidence/sdk";
+import { ConfidenceProvider } from '@spotify-confidence/react';
+import { ReactNode } from 'react';
+import { Confidence, visitorIdentity } from '@spotify-confidence/sdk';
 
 if (!process.env.NEXT_PUBLIC_REACT_APP_CLIENT_SECRET) {
-    console.error('NEXT_PUBLIC_REACT_APP_CLIENT_SECRET not set in .env');
-    process.exit(1);
+  console.error('NEXT_PUBLIC_REACT_APP_CLIENT_SECRET not set in .env');
+  process.exit(1);
 }
 
 const cf = Confidence.create({
-    clientSecret: process.env.NEXT_PUBLIC_REACT_APP_CLIENT_SECRET,
-    environment: 'client',
-    region: 'eu',
-    timeout: 1000,
-    logger: console,
+  clientSecret: process.env.NEXT_PUBLIC_REACT_APP_CLIENT_SECRET,
+  environment: 'client',
+  region: 'eu',
+  timeout: 1000,
+  logger: console,
 });
 
 cf.track(visitorIdentity());
 //cf.setContext({visitor_id: 'new_button_style_visitor'})
-cf.setContext({visitor_id: 'default_button_style_visitor'})
+cf.setContext({ visitor_id: 'default_button_style_visitor' });
 //cf.setContext({ visitor_id: visitorIdentity().toString() })
 
-
 type RootConfProviderProps = {
-    children: ReactNode,
-}
+  children: ReactNode;
+};
 
-export default function RootConfProvider({children}:RootConfProviderProps) {
-    return (
-        <ConfidenceProvider confidence={cf}>
-            {children}
-        </ConfidenceProvider>
-    )
+export default function RootConfProvider({ children }: RootConfProviderProps) {
+  return <ConfidenceProvider confidence={cf}>{children}</ConfidenceProvider>;
 }
--- examples/nextjs/src/components/Flags.tsx
+++ examples/nextjs/src/components/Flags.tsx
@@ -1,19 +1,18 @@
-'use client'
-import {useConfidence} from "@spotify-confidence/react";
+'use client';
+import { useConfidence } from '@spotify-confidence/react';
 
 export type ButtonStyleFlag = {
-    inverted: boolean;
-}
+  inverted: boolean;
+};
 
 export function Flags() {
-    const confidence = useConfidence();
-    const flagData = JSON.stringify(confidence.useEvaluateFlag('button-style',
-        {"inverted": false}), null, '  ');
-    // const flagData = useDeferredValue(confidence.useFlag('web-sdk-e2e-flag.str', 'default'));
-    return (
-        <fieldset>
-            <legend>Flags</legend>
-            <pre>{flagData}</pre>
-        </fieldset>
-    );
+  const confidence = useConfidence();
+  const flagData = JSON.stringify(confidence.useEvaluateFlag('button-style', { inverted: false }), null, '  ');
+  // const flagData = useDeferredValue(confidence.useFlag('web-sdk-e2e-flag.str', 'default'));
+  return (
+    <fieldset>
+      <legend>Flags</legend>
+      <pre>{flagData}</pre>
+    </fieldset>
+  );
 }
